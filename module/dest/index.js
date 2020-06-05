d49f9a71117a9a3278cf0a2620d66262315b5a46
Pein Freecs <plants.coordinators@gmail.com>
2020-06-05 22:33:58 +0800

    changing the architecture for grasseum, to support modular structure

---
+++ module/dest/index.js
@@ -0,0 +1,7 @@
+function destcDir(dir,file,func){
+
+}
+
+module.exports = function( dir,file,func){
+    var n =new destcDir(dir,file,func)
+}
--- write_file.js
+++ write_file.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+let compt  = require("compt")
 exports.write_file= async function(src,data,func){
     fs.writeFile(src, data,function(err){
         
-            
+        if(compt._.has(func))    
         func({"error":err})
         
     });
