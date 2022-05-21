e765efa9e6ef28ab3f67eb15b742bbd9934d34ab
codehyouka <plants.coordinators@gmail.com>
2022-05-21 21:11:04 +0800

    Major modification in code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var module_src_index =require("grasseum_directory/module/directory")
-var compt = require("compts");
-var fs = require('fs');
+exports.directory = function () {
 
-exports.directory = function(){
-    return require("grasseum_directory/module/directory")
-}
+    return require("grasseum_directory/module/directory");
+
+};
 
--- module/directory/index.js
+++ module/directory/index.js
@@ -1,49 +1,98 @@
-var validation = require("grasseum_glob/util/validation");
-var compt = require("compts");
-var fs = require('fs');
-var path = require('path');
+const {unixpath} = require('path-assist');
+const structkit = require("structkit");
+const fs = require('fs');
+const path = require('path');
 
-exports.createFolder = function(location,succfunc,errorfunc){
-    if (!fs.existsSync(location)){
-        try{
-         fs.mkdirSync(location);
-         succfunc({"status":"folder_created"})
-        }catch(e){
-            errorfunc(e);
+const folder_creator = function (pathname, counter, succfunc, errorfunc) {
+
+    exports.createFolder(structkit.toArray(structkit.getValue(structkit.limit(pathname, 0, counter))).join("/"), function (data) {
+
+        counter+=1;
+        if (counter< pathname.length) {
+
+            folder_creator(pathname, counter, succfunc, errorfunc);
+
+        }else{
+            succfunc(data);
         }
-         
-     }else{
-        succfunc({"status":"folder_existed"})
-     }
-}
-
-exports.createFolderRecursivelyIfNotExist = function(location){
-    var pathname = validation.cleanDirPath(path.dirname(location)).split("/");
-    var counter =0;
-   
-    if (!fs.existsSync(location)){
         
-        folder_creator(pathname,counter,function(data){
-   
-        },function(data){
 
-        });
-    }
-}
+    }, function (data) {
 
-
-var folder_creator = function(pathname,counter,succfunc,errorfunc){
-    exports.createFolder(compt._.to_array(compt._.getValue(compt._.limit(pathname,0,counter))).join("/"),function(data){
-        if(data["status"] == "folder_created"){
-
-        }
-        counter++;
-        if( counter< pathname.length){
-            folder_creator(pathname,counter,succfunc,errorfunc);
-        }
-        succfunc(data);
-    },function(data){
-        console.log(data,"error");
+        console.log(data, "error");
         errorfunc(data);
+
     });
-}
+
+};
+
+exports.createFolder = function (location, succfunc, errorfunc) {
+
+    fs.access(location, fs.constants.F_OK, (error) => {
+
+     
+        if(error ==null){
+            succfunc({"status": "folder_existed"});
+        }else{
+            try {
+
+                /*
+                 * Fs.mkdirSync(location);
+                 * Succfunc({"status": "folder_created"});
+                 */
+                fs.mkdir(location, {recursive: true}, (err) => {
+    
+                    if (err) {
+    
+                        throw err;
+    
+                    }
+                    succfunc({"status": "folder_created"});
+    
+                });
+    
+            } catch (exception) {
+    
+                errorfunc(exception);
+    
+            }
+        }
+        
+
+    });
+
+};
+
+exports.createFolderRecursivelyIfNotExist = function (location ,isAutoGenerateFolder,successFunc,failedFunc) {
+
+    const pathname = unixpath.cleanDirPath(path.dirname(location)).split("/");
+    const counter =0;
+
+
+    fs.access(location, fs.constants.F_OK, () => {
+
+        if(isAutoGenerateFolder){
+            folder_creator(pathname, counter, function () {
+ 
+               if(structkit.has(successFunc)){
+                successFunc()
+               }
+            
+            }, function () {
+ 
+            if(structkit.has(failedFunc)){
+                failedFunc()
+               }
+    
+            });
+        }else{
+            if(structkit.has(successFunc)){
+                successFunc()
+            }
+        }
+        
+
+    });
+
+};
+
